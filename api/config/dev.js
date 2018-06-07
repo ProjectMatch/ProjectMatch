ab0510d1c4e6ec03471e8d00b19f7e5c944ba27d
Tony Luo <luo.tony37@gmail.com>
2018-06-07 13:22:20 -0700

    Set up dynamic config

---
+++ api/config/dev.js
@@ -0,0 +1,9 @@
+const config = {
+  port: 8080,
+  secrets: {},
+  db: {
+    url: 'mongodb://localhost/project-match'
+  }
+};
+
+module.exports = config;
---
+++ api/config/index.js
@@ -0,0 +1,20 @@
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+const env = process.env.NODE_ENV;
+
+let config = {};
+
+switch (env) {
+  case 'dev':
+    config = require('./dev');
+    break;
+  case 'testing':
+    config = require('./testing');
+    break;
+  case 'production':
+    config = require('./prod');
+  default:
+    config = require('./dev');
+}
+
+module.exports = config;
