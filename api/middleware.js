da5f21c439246d93396f577cbc3635086fa1735e
Tony Luo <luo.tony37@gmail.com>
2018-06-07 14:22:19 -0700

    Separate out middleware into own file

---
+++ api/middleware.js
@@ -0,0 +1,37 @@
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const passport = require('./passport');
+const morgan = require('morgan');
+const config = require('./utils/config');
+
+function setupMiddleware(app) {
+  app.use(
+    cors({
+      origin: config.host.name,
+      credentials: true,
+      preflightContinue: true,
+      optionsSuccessStatus: 200
+    })
+  );
+  app.use(morgan('dev'));
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(cookieParser());
+  app.use(
+    session({
+      secret: config.sessionSecret,
+      resave: false, // forces session to be saved even when there was no change
+      saveUninitialized: false, // forces uninitialized sessions to be saved
+      cookie: {
+        maxAge: 6000000,
+        httpOnly: false
+      }
+    })
+  );
+  app.use(passport.initialize());
+  app.use(passport.session());
+}
+
+module.exports = setupMiddleware;
--- api/server.js
+++ api/server.js
@@ -1,43 +1,11 @@
 const express = require('express');
-const cors = require('cors');
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
-const session = require('express-session');
-const passport = require('./passport');
-const morgan = require('morgan');
-const config = require('./utils/config');
+const setupMiddlewares = require('./middleware');
 const apiRouter = require('./apiRouter');
 const connect = require('./db');
 const app = express();
 
 connect();
-
-app.use(
-  cors({
-    origin: config.host.name,
-    credentials: true,
-    preflightContinue: true,
-    optionsSuccessStatus: 200
-  })
-);
-app.use(morgan('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cookieParser());
-app.use(
-  session({
-    secret: config.sessionSecret,
-    resave: false, // forces session to be saved even when there was no change
-    saveUninitialized: false, // forces uninitialized sessions to be saved
-    cookie: {
-      maxAge: 6000000,
-      httpOnly: false
-    }
-  })
-);
-
-app.use(passport.initialize());
-app.use(passport.session());
+setupMiddlewares(app);
 
 // Router
 app.use('/api', apiRouter);
