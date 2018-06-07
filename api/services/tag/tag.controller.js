e552665033af78f5eaee11bdaec528071526eeb6
Tony Luo <luo.tony37@gmail.com>
2018-06-07 16:12:16 -0700

    Fix addTag controller

--- api/services/tag/tag.controller.js
+++ api/services/tag/tag.controller.js
@@ -11,57 +11,19 @@ function getTags(req, res) {
   });
 }
 
-function setupTags(req, res) {
-  const tagArray = [
-    'web app',
-    'mobile app',
-    'machine learning',
-    'data science',
-    'finances',
-    'food',
-    'budgeting',
-    'bot',
-    'travel',
-    'portfolio',
-    'mockups',
-    'collaboration',
-    'weather',
-    'chrome extension',
-    'anime',
-    'chingu',
-    'landing page',
-    'music',
-    'chat',
-    'fitness',
-    'game'
-  ];
-
-  saveNewTag = tagName => {
-    const newTag = new Tags({ tagName: tagName });
-    newTag.save(function(err) {
-      if (err) {
-        console.log('Error in saving tag: ' + err);
-      }
-    });
-  };
-
-  async function mapThenSaveTags() {
-    await tagArray.map(tag => {
-      saveNewTag(tag);
-    });
-    return Tags.find({}, function(err, tags) {
-      if (err || !tags) {
-        res.json({ error: 'Error in saving and retrieving tags: ' + err });
-      } else {
-        res.json({ tags: tags, message: 'Successfully saved batch tags' });
-      }
-    });
-  }
-
-  mapThenSaveTags();
+function addTag(req, res) {
+  const name = req.body.name;
+  const newTag = new Tags({ name });
+  newTag.save(function(err) {
+    if (err) {
+      res.json({ error: 'Could not add tag' });
+    } else {
+      res.json({ message: 'Tag successfully added' });
+    }
+  });
 }
 
 module.exports = {
   getTags,
-  setupTags
+  addTag
 };
--- api/services/tag/tag.router.js
+++ api/services/tag/tag.router.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const controller = require('./tag.controller');
 
 router.get('/', controller.getTags);
-router.get('/setup', controller.setupTags);
+router.get('/add', controller.addTag);
 
 module.exports = router;
