9fe0f151be4617973ea492da35305757c4492466
Tony Luo <luo.tony37@gmail.com>
2018-06-07 13:14:14 -0700

    Rename auth helper file

--- api/utils/auth.js
+++ api/utils/auth.js
@@ -1,9 +1,8 @@
 const isAuthenticated = function(req, res, next) {
   if (req.isAuthenticated()) {
-    console.log('You are authorized');
     return next();
   }
-  res.send({ message: 'You are not authorized' });
+  res.json({ error: 'unauthorized user', message: 'You are not authorized' });
 };
 
 module.exports = isAuthenticated;
