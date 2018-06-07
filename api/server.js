2a0889e1fa0c1e090e9a49d28188cc5c926102fd
Tony Luo <luo.tony37@gmail.com>
2018-06-07 14:27:00 -0700

    Update 404 and 5xx handlers

--- api/server.js
+++ api/server.js
@@ -10,14 +10,15 @@ setupMiddlewares(app);
 // Router
 app.use('/api', apiRouter);
 
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.json({
-      message: err.message,
-      error: err
-    });
-  });
-}
+// 404 Handler
+app.use(function(req, res, next) {
+  res.sendStatus(404);
+});
+
+// 500 Handler
+app.use(function(err, req, res, next) {
+  console.error(err.stack);
+  res.sendStatus(500);
+});
 
 module.exports = app;
