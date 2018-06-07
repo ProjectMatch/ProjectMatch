17c17203df4f8f59659b21bff5bf25af876d7df4
Tony Luo <luo.tony37@gmail.com>
2018-06-07 16:06:46 -0700

    Refactor download controller

--- api/services/download/download.controller.js
+++ api/services/download/download.controller.js
@@ -4,27 +4,21 @@ AWS.config.update(aws_secret);
 const s3 = new AWS.S3();
 
 function getProfileImage(req, res) {
-  console.log('got the request for profile');
-
   const urlParams = {
     Bucket: 'project-match',
     Key: 'profile/' + req.query.userName
   };
-  console.log(urlParams);
+
   s3.getSignedUrl('getObject', urlParams, function(err, url) {
     if (err) {
-      res.json({ error: err.message });
+      res.json({ err });
     }
-    console.log('the url of the image is', url);
-    res.json({ url: url, message: 'Successfully retrieved profile image URL' });
+    res.json({ url, message: 'Successfully retrieved profile image URL' });
   });
 }
 
 function getProjectImages(req, res) {
-  console.log('got the request for project');
-
-  const getListObject = new Promise(function(resolve, reject) {
-    console.log('getListObject()');
+  const getListPromise = new Promise(function(resolve, reject) {
     const params = {
       Bucket: 'project-match',
       Delimiter: '/',
@@ -32,22 +26,18 @@ function getProjectImages(req, res) {
     };
     s3.listObjects(params, function(err, data) {
       if (err) {
-        console.log('got error');
         reject(err);
       }
-      console.log('got data');
       resolve(data.Contents);
     });
   });
 
-  const geturlsArry = function(data) {
+  const getURLArray = function(data) {
     return new Promise(function(resolve, reject) {
-      console.log('geturlsArry()');
-
       const urls = [];
 
-      for (const i = 0; i < data.length; i++) {
-        const urlParams = { Bucket: 'project-match', Key: data[i].Key };
+      for (let i = 0; i < data.length; i++) {
+        let urlParams = { Bucket: 'project-match', Key: data[i].Key };
         s3.getSignedUrl('getObject', urlParams, function(err, url) {
           if (err) {
             return reject(err);
@@ -56,17 +46,15 @@ function getProjectImages(req, res) {
         });
       }
 
-      setTimeout(() => {
-        if (urls.length == data.length) {
-          resolve(urls);
-        }
-      }, 100);
+      if (urls.length === data.length) {
+        resolve(urls);
+      }
     });
   };
 
-  getListObject
+  getListPromise
     .then(function(data) {
-      geturlsArry(data).then(function(urls) {
+      getURLArray(data).then(function(urls) {
         res.json({
           urls: urls,
           message: 'Successfully retrieved project image URL'
