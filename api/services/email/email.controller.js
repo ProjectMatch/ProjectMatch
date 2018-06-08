fd4d5b74fc79a9ec5951a3d2f3d8d63460d4b69b
Tony Luo <luo.tony37@gmail.com>
2018-06-07 18:26:30 -0700

    Update email controller

--- api/services/email/email.controller.js
+++ api/services/email/email.controller.js
@@ -3,13 +3,61 @@ const Project = require('../models/Projects');
 const User = require('../models/Users');
 const config = require('../utils/config');
 
-function sendRequestToJoinTeam(req, res) {
-  const link = req.body.link;
-  const sender = req.body.username;
-  const projectName = req.body.projectName;
-  const projectId = req.body.projectId;
-  const interestedParty = req.body.username;
+const SMTP_HOST = 'smtp.sendgrid.net';
+const PORT = 587;
+const SECURE = false;
+const USER = 'apikey';
+const PASS =
+  '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';
 
+const FROM_EMAIL = 'join_request@projectmatch.me';
+const EMAIL_SUBJECT = 'Project Match - Join Request';
+
+function createSMTPTransport() {
+  return nodemailer.createTransport({
+    host: SMTP_HOST,
+    port: PORT,
+    secure: SECURE,
+    auth: {
+      user: USER,
+      pass: PASS
+    }
+  });
+}
+
+function sendEmailRequest(err, res, user, info) {
+  const sender = info.username;
+  const projectName = info.projectName;
+  const projectId = info.projectId;
+  const interestedParty = info.username;
+
+  if (err) {
+    res.send(409);
+  } else {
+    const smtpTransport = createSMTPTransport();
+    const mailerOptions = {
+      to: user.email,
+      from: FROM_EMAIL,
+      subject: EMAIL_SUBJECT,
+      text: `Hello,\n\nYou've got a request from ${sender} to join ${projectName}.\n\nYou can see their profile here: ${link}.\n\nTo accept the request, click: ${
+        config.server.name
+      }/api/projects/${projectId}/accept/${interestedParty}`
+    };
+
+    smtpTransport.sendMail(mailerOptions, function(err) {
+      if (err) {
+        res.json({
+          error: err.message
+        });
+      }
+      res.json({
+        success: 'Email sent'
+      });
+    });
+  }
+}
+
+function sendRequestToJoinTeam(req, res) {
   Project.findById(projectId, function(err, project) {
     if (err) {
       res.send(409);
@@ -18,44 +66,7 @@ function sendRequestToJoinTeam(req, res) {
         {
           username: project.creator
         },
-        function(err, user) {
-          if (err) {
-            res.send(409);
-          } else {
-            const to = user.email;
-
-            const smtpTransport = nodemailer.createTransport({
-              host: 'smtp.sendgrid.net',
-              port: 587,
-              secure: false, // true for 465, false for other ports
-              auth: {
-                user: 'apikey', // generated ethereal user
-                pass:
-                  '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' // generated ethereal password
-              }
-            });
-
-            const mailOptions = {
-              to: to,
-              from: 'join_request@projectmatch.me',
-              subject: 'Project Match - Join Request',
-              text: `Hello,\n\nYou've got a request from ${sender} to join ${projectName}.\n\nYou can see their profile here: ${link}.\n\nTo accept the request, click: ${
-                config.server.name
-              }/api/projects/${projectId}/accept/${interestedParty}`
-            };
-
-            smtpTransport.sendMail(mailOptions, function(err) {
-              if (err) {
-                res.json({
-                  error: err.message
-                });
-              }
-              res.json({
-                success: 'Success!'
-              });
-            });
-          }
-        }
+        sendEmailRequest(err, res, user, req.body)
       );
     }
   });
