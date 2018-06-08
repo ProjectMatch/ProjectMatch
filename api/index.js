a7821e115e611e52cc342c0b9ac7f78699e73f94
Tony Luo <luo.tony37@gmail.com>
2018-06-07 19:19:52 -0700

    Update server listen to be dynamically dependent on config

--- api/index.js
+++ api/index.js
@@ -1,5 +1,6 @@
+const config = require('./config');
 const server = require('./server');
 
-server.listen(8080, function() {
-  console.log('CORS-enabled web server listening on port 8080');
+server.listen(config.port, function() {
+  console.log(`Express server listening on port ${config.port}`);
 });
