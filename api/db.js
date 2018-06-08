3ab484e85ecdbcfb99e52c6d3d430643237673bc
Tony Luo <luo.tony37@gmail.com>
2018-06-07 19:17:21 -0700

    Remove depreciated mongoose options

--- api/db.js
+++ api/db.js
@@ -2,12 +2,7 @@ const mongoose = require('mongoose');
 const config = require('./config');
 
 const connect = () => {
-  return mongoose.connect(config.db.url, {
-    server: {
-      reconnectTries: Number.MAX_VALUE,
-      reconnectInterval: 1000
-    }
-  });
+  return mongoose.connect(config.db.url);
 };
 
 module.exports = connect;
