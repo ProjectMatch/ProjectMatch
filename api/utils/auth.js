5cd7657480e2b2cb08dae2994b8e3af1aa79d21a
Tony Luo <luo.tony37@gmail.com>
2018-06-28 12:06:16 -0700

    Set status to unauthorized on failed authentication

--- api/utils/auth.js
+++ api/utils/auth.js
@@ -2,7 +2,9 @@ const isAuthenticated = function(req, res, next) {
   if (req.isAuthenticated()) {
     return next();
   }
-  res.json({ error: 'unauthorized user', message: 'You are not authorized' });
+  res
+    .status(401)
+    .json({ error: 'unauthorized user', message: 'You are not authorized' });
 };
 
 module.exports = isAuthenticated;
