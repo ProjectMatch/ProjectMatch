2d94fb440ed03c257b247f42d2a86302259bc58d
Tony Luo <luo.tony37@gmail.com>
2018-06-29 12:25:23 -0700

    Rename services to resources

--- api/apiRouter.js
+++ api/apiRouter.js
@@ -1,17 +1,17 @@
 const express = require('express');
 const router = express.Router();
 
-router.use('/auth', require('./services/auth'));
-router.use('/forgot', require('./services/password'));
-router.use('/reset', require('./services/password'));
-router.use('/user/update', require('./services/user'));
-router.use('/user', require('./services/user'));
-router.use('/projects/tags', require('./services/tag'));
-router.use('/projects/categories', require('./services/category'));
-router.use('/projects', require('./services/project'));
-router.use('/upload/image', require('./services/image'));
-router.use('/download', require('./services/download'));
-router.use('/email', require('./services/email'));
+router.use('/auth', require('./resources/auth'));
+router.use('/forgot', require('./resources/password'));
+router.use('/reset', require('./resources/password'));
+router.use('/user/update', require('./resources/user'));
+router.use('/user', require('./resources/user'));
+router.use('/projects/tags', require('./resources/tag'));
+router.use('/projects/categories', require('./resources/category'));
+router.use('/projects', require('./resources/project'));
+router.use('/upload/image', require('./resources/image'));
+router.use('/download', require('./resources/download'));
+router.use('/email', require('./resources/email'));
 router.use(function(req, res, next) {
   res.sendStatus(404);
 });
